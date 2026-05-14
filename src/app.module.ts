749a73d0829447c85c23d4c603026b8ea7955e56
Diego Guzman <diegoguzman366@gmail.com>
2026-05-14 15:25:14 -0400

    fix build

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { OrdersModule } from './modules/orders/orders.module';
 import { UsersModule } from './modules/users/users.module';
-import { TestModule } from './test/test.module';
-import { CatsModule } from './cats/cats.module';
+
 
 @Module({
-  imports: [AuthModule, UsersModule, OrdersModule, TestModule, CatsModule],
+  imports: [AuthModule, UsersModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
