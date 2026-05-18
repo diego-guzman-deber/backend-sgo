37396003c9d054af0eb2758da66b094967044598
Diego Guzman <diego.guzman@grupoeldeber.com>
2026-05-18 11:13:21 -0400

    fix: update CORS origin to use HTTPS for production

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: process.env.CORS_ORIGIN ?? "http://sgo.api.eldeber.bo" ,
+    origin: process.env.CORS_ORIGIN ?? "https://sgo.api.eldeber.bo" ,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     exposedHeaders: ['Content-Disposition'], // necesario para que el frontend lea el nombre del archivo Excel
